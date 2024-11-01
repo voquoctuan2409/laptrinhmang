6cdd3e4aa90130184cdf219a8fa958e9b03c86ba
hieuknguyen <89458362+hieuknguyen@users.noreply.github.com>
2024-11-01 18:34:31 +0700

    Add files via upload

---
+++ reamail/ReadEmailDemo/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadEmailDemo
+{
+    public static class Constants
+    {
+        public const string EMAIL = "nguyentrunghieu4390@gmail.com";
+        public const string PASSWORD = "uenx fkai gehq opcb";
+        public const string FOLDER_READ = "Inbox";
+    }
+}
